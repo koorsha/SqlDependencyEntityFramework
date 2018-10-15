dbdbcb76f1d0e2d5de11c8b6476a24e39bcdf9d5
Koorsha <koorshashirazi@yahoo.com>
2018-10-15 12:13:45 +0200

    Add files via upload

---
+++ CSEFAutoUpdate/WarehouseContext.cs
@@ -0,0 +1,36 @@
+﻿/****************************** Module Header ******************************\
+Module Name:  WarehouseContext.cs
+Project:      CSEFAutoUpdate
+Copyright (c) Microsoft Corporation.
+
+We can use the Sqldependency to get the notification when the data is changed 
+in database, but EF doesn’t have the same feature. In this sample, we will 
+demonstrate how to automatically update by Sqldependency in Entity Framework.
+In this sample, we will demonstrate two ways that use SqlDependency to get the 
+change notification to auto update data.
+It's the context class used in EF.
+
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+All other rights reserved.
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Data.Entity;
+
+namespace CSEFAutoUpdate
+{
+    /// <summary>
+    /// https://code.msdn.microsoft.com/How-to-use-SqlDependency-5c0da0b3#content
+    /// active sql dependcy in sql server
+    /// ALTER DATABASE [Warehouse] SET ENABLE_BROKER WITH ROLLBACK IMMEDIATE
+    /// </summary>
+    public class WarehouseContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
